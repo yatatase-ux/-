4eac13153a8a94851dec159f2d5ab56f151619da
4D-09\koyo <a>
2026-05-26 14:00:13 +0900

    コンストラクタの改正。変数が多くなった時のデフォルトの設定方法の追加。Show関数でアウトプットを簡単に

--- コンストラクタ/コンストラクタ.cpp
+++ コンストラクタ/コンストラクタ.cpp
@@ -7,25 +7,42 @@ public:
 
 	std::string NAME;   // プレイヤーの名前
 	int HP;             // プレイヤーのHP
+    int A;
+    int B;
+    int C;
+    int D;
+    int S;
 
     // デフォルトコンストラクタ
     Player()
-        : Player("Default", 100)
+        : Player("Default")
     {
     }
 
     // コンストラクタ
-    Player(std::string name, int hp)
-        : NAME(name), HP(hp)
+    Player(std::string name, int hp = 108, int a = 130, int b = 95, int c = 80, int d = 85, int s = 102)
+        : NAME(name), HP(hp), A(a), B(b), C(c), D(d), S(s) 
     {
     }
 };
 
+void Show(const Player& player)
+{
+    std::cout << "NAME : " << player.NAME << std::endl
+        << "H : " << player.HP << std::endl
+		<< "A : " << player.A << std::endl
+		<< "B : " << player.B << std::endl
+		<< "C : " << player.C << std::endl
+		<< "D : " << player.D << std::endl
+		<< "S : " << player.S << std::endl
+        << std::endl;
+}   
+
 int main()
 {
     Player player1;
-    Player player2("勇者", 200);
+    Player player2("勇者", 75, 65, 72, 159, 125, 139);
 
-    std::cout << "NAME : " << player1.NAME << ", HP : " << player1.HP << std::endl;
-    std::cout << "NAME : " << player2.NAME << ", HP : " << player2.HP << std::endl;
+	Show(player1);
+    Show(player2);
 }
