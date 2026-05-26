aa6c690c0aeab2a91e8ce60179ca2b73b5397a67
4D-09\koyo <a>
2026-05-26 13:45:45 +0900

    ・デフォルトコンストラクタの追加 ・NAMEの追加 ・player1はデフォルトコンストラクタを、player2は引数ありコンストラクタを使用

--- コンストラクタ/コンストラクタ.cpp
+++ コンストラクタ/コンストラクタ.cpp
@@ -1,19 +1,30 @@
 ﻿#include <iostream>
+#include <string>  
 
 class Player
 {
 public:
+    std::string NAME;
     int HP;
-    // コンストラクタ
-    Player(int hp) : HP(hp)
-    {
 
+    Player()
+        : Player("Default", 100)
+    {
+    }
+
+    // コンストラクタ
+    Player(std::string name, int hp)
+        : NAME(name), HP(hp)
+    {
     }
 };
 
 int main()
 {
-    Player player(50);
+    Player player1;
+    Player player2("勇者", 200);
 
-    std::cout << "HP : " << player.HP << std::endl;
+
+    std::cout << "NAME : " << player1.NAME << ", HP : " << player1.HP << std::endl;
+    std::cout << "NAME : " << player2.NAME << ", HP : " << player2.HP << std::endl;
 }
